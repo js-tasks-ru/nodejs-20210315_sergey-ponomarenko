0e055344b4b97b74b4e3376563987489fca9f5b9
Sergey Ponomarenko <xto3na@gmail.com>
2021-03-28 12:02:41 +0300

    Line split stream solution.

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -2,14 +2,26 @@ const stream = require('stream');
 const os = require('os');
 
 class LineSplitStream extends stream.Transform {
+  #lastLine = '';
   constructor(options) {
     super(options);
   }
 
   _transform(chunk, encoding, callback) {
+    let tempString = `${this.#lastLine}${chunk.toString()}`;
+    if (tempString.includes(os.EOL)) {
+      const [line, ...rest] = tempString.split(os.EOL);
+      this.push(line);
+      this.#lastLine = rest.join(os.EOL);
+    } else {
+      this.#lastLine = tempString;
+    }
+    callback();
   }
 
   _flush(callback) {
+    this.#lastLine.split(os.EOL).forEach((item) => this.push(item));
+    callback();
   }
 }
 
