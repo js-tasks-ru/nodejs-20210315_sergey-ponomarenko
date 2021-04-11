78e45543d016c30aa3a2f4de80df0a4be8053ee8
Sergey Ponomarenko <xto3na@gmail.com>
2021-04-11 16:18:12 +0300

    Solution for file delete.

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,19 +1,35 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+  const pathname = new url.URL(req.url, `${req.protocol}://${req.headers.host}`).pathname.slice(1);
+  if (!pathname || pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('Bad request');
+    return false;
+  }
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
-
+      try {
+        if (fs.existsSync(filepath)) {
+          fs.unlinkSync(filepath);
+          res.statusCode = 200;
+          res.end('The file was deleted.');
+        } else {
+          res.statusCode = 404;
+          res.end('Not found');
+        }
+      } catch (error) {
+        console.log(error);
+      }
       break;
-
     default:
       res.statusCode = 501;
       res.end('Not implemented');
