2a97b0c0c9fb829e2f1132e63d6ea8a35b6cc2db
Sergey Ponomarenko <xto3na@gmail.com>
2021-04-11 17:09:37 +0300

    Solution for koajs chat.

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,42 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let chatMembers = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  const promise = new Promise((resolve, reject) => {
+    chatMembers.push(resolve);
+    ctx.res.on('close', () => {
+      chatMembers.splice(chatMembers.indexOf(resolve), 1);
+      const error = new Error('Connection closed');
+      error.code = 'ECONNRESET';
+      reject(error);
+    });
+  });
+
+  let message = '';
+
+  try {
+    message = await promise;
+  } catch (err) {
+    if (err.code === 'ECONNRESET') {
+      return;
+    }
+    throw err;
+  }
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+  if (!message) {
+    ctx.throw(400);
+  }
+  chatMembers.forEach( (resolve) => {
+    resolve(String(message));
+  });
+  chatMembers = [];
+  ctx.body = 'ok';
 });
 
 app.use(router.routes());
