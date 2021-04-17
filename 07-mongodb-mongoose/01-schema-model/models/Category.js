0c420b013e4332df7c0136285e66f365cf86368f
Sergey Ponomarenko <xto3na@gmail.com>
2021-04-17 12:18:47 +0300

    Schema model solution.

--- 07-mongodb-mongoose/01-schema-model/models/Category.js
+++ 07-mongodb-mongoose/01-schema-model/models/Category.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const subCategorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 });
 
 const categorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   subcategories: [subCategorySchema],
 });
 
