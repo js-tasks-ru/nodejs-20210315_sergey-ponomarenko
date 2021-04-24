53e80a46baaf422816b8fea61a32c3580839bd98
Sergey Ponomarenko <xto3na@gmail.com>
2021-04-24 19:49:34 +0300

    Solution for rest-api.

--- 07-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,12 +1,48 @@
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
+
+const converter = (product) => {
+  return {
+    id: product.id,
+    title: product.title,
+    images: product.images,
+    category: product.category,
+    subcategory: product.subcategory,
+    price: product.price,
+    description: product.description,
+  };
+};
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  const subcategory = ctx.query;
+  if (!subcategory) {
+    return next();
+  }
+  const products = await Product.find({subcategory: subcategory});
+  ctx.body = {
+    products: products.map(converter),
+  };
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find();
+  ctx.body = {
+    products: products.map(converter),
+  };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  if (!mongoose.Types.ObjectId.isValid(ctx.params.id)) {
+    ctx.throw(400, 'Invalid id');
+  }
+
+  const product = await Product.findById(ctx.params.id);
+
+  if (!product) {
+    ctx.throw(404, 'Product not found!');
+  }
+  ctx.body = {
+    product: converter(product),
+  };
 };
 
